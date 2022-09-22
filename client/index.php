c3fcec246ec56a5bf08dbadd9083a15b8a716b57
Nahuel61920 <estebancarrizo619@gmail.com>
2022-09-21 23:34:28 -0300

    back end 100%

---
+++ client/index.php
@@ -0,0 +1,60 @@
+<?php
+
+include 'db/db.php';
+
+header('Access-Control-Allow-Origin: *');
+
+if($_SERVER['REQUEST_METHOD']=='GET'){
+    if(isset($_GET['id'])){
+        $query="select * from lenguages where id=".$_GET['id'];
+        $resultado=metodoGet($query);
+        echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
+    }else{
+        $query="select * from lenguages";
+        $resultado=metodoGet($query);
+        echo json_encode($resultado->fetchAll()); 
+    }
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+if($_POST['METHOD']=='POST'){
+    unset($_POST['METHOD']);
+    $name=$_POST['name'];
+    $launch=$_POST['launch'];
+    $developer=$_POST['developer'];
+    $query="insert into lenguages(name, launch, developer) values ('$name', '$launch', '$developer')";
+    $queryAutoIncrement="select MAX(id) as id from lenguages";
+    $resultado=metodoPost($query, $queryAutoIncrement);
+    echo json_encode($resultado);
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+if($_POST['METHOD']=='PUT'){
+    unset($_POST['METHOD']);
+    $id=$_GET['id'];
+    $name=$_POST['name'];
+    $launch=$_POST['launch'];
+    $developer=$_POST['developer'];
+    $query="UPDATE lenguages SET name='$name', launch='$launch', developer='$developer' WHERE id='$id'";
+    $resultado=metodoPut($query);
+    echo json_encode($resultado);
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+if($_POST['METHOD']=='DELETE'){
+    unset($_POST['METHOD']);
+    $id=$_GET['id'];
+    $query="DELETE FROM lenguages WHERE id='$id'";
+    $resultado=metodoDelete($query);
+    echo json_encode($resultado);
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+header("HTTP/1.1 400 Bad Request");
+
+
+?>
